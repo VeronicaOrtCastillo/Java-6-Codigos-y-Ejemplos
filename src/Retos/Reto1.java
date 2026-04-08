cde2e4900c5250af4a87021e41452c6943221def
VeronicaOrtCastillo <veronicaortcaz@gmail.com>
2026-04-07 19:06:32 -0600

    Se agrega el contenido del Reto1

--- src/Retos/Reto1.java
+++ src/Retos/Reto1.java
@@ -1,4 +1,20 @@
 package Retos;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Reto1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Permite crear un objeto para generar numeros aleatorios
+        Random random = new Random();
+
+        System.out.println("Ingresa el numero de participantes: ");
+        int participantes = sc.nextInt();
+
+        //Genera un numero aleatorio entre 1 y el ingresado por el usuario
+        int ganador = random.nextInt(participantes) + 1;
+
+        System.out.println("El numero ganador es: "+ ganador);
+    }
 }
